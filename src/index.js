626b80b17fff115257c95079ea94e644cc64d72e
kwojnar137 <k.wojnar137@gmail.com>
2020-07-10 17:34:11 +0200

    Storybook - przyklad z oficjalnego poradnika

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/index.css';
+import App from './App';
+import Task from './components/Task'
+
+
+
+ReactDOM.render(
+  <Task />,
+  document.getElementById('root')
+);
+
