7d43286b74e02b92f791f032e6c61098ee7db07e
kwojnar137 <k.wojnar137@gmail.com>
2020-09-08 18:34:17 +0200

    Updates

---
+++ precache-manifest.2dd4e8618b5888ba568d0a870493836a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5e124c53dc54f6e1e749de88ff8c9a4",
+    "url": "/skillstree/index.html"
+  },
+  {
+    "revision": "bbb65739d5a28368c71c",
+    "url": "/skillstree/static/css/main.1bc0d039.chunk.css"
+  },
+  {
+    "revision": "008a6d723facfe0dd9e3",
+    "url": "/skillstree/static/js/2.f0f73a80.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/skillstree/static/js/2.f0f73a80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbb65739d5a28368c71c",
+    "url": "/skillstree/static/js/main.2cbc206b.chunk.js"
+  },
+  {
+    "revision": "962949bb9e57e98a171f",
+    "url": "/skillstree/static/js/runtime-main.563dd6d6.js"
+  },
+  {
+    "revision": "682eec3813bd9370550e986555c2bf02",
+    "url": "/skillstree/static/media/MenuIcon.682eec38.svg"
+  },
+  {
+    "revision": "ac23faf4178cfbf299810ffe50ea2dd7",
+    "url": "/skillstree/static/media/cancelMenu.ac23faf4.svg"
+  }
+]);
