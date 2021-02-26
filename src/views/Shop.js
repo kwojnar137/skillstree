3d03d2ea636348c9cbcb1941f8bc19e8d9025f40
kwojnar137 <k.wojnar137@gmail.com>
2021-02-26 11:37:54 +0100

    add bubble

--- src/views/Shop.js
+++ src/views/Shop.js
@@ -3,22 +3,25 @@ import React from "react";
 import Layout from "../layout";
 
 const mystyle = {
-  border: "0",
+  border: "0px",
   width: "100%!important",
-  padding: "0",
+  padding: "0px",
   margin: "0 auto",
   minHeight: "200px",
   maxWidth: "1000px",
   display: "block"
 };
 
+
+
 // const style={{border: "0", width: '100%!important', padding: '0', margin: '0 auto', minHeight: '200px', maxWidth: '1000px', display: 'block'}} 
-const script = <div><iframe id="dl-widget" style={mystyle} src='https://widget-sandbox.droplabs.pl/index.html?facilityId=1&onlineGroupId=3421'></iframe><script type="text/javascript" src="https://widget-sandbox.droplabs.pl/widget.js"></script></div>
+const script = <div><iframe id="dl-widget" style={mystyle} src='https://widget.droplabs.pl/index.html?facilityId=235&onlineGroupId=5041'></iframe><script type="text/javascript" src="https://widget.droplabs.pl/widget.js"></script></div>
 
 export default function Shop() {
   return (
     <Layout>
-      {script}
+      {/* {script} */}
+      <script type="text/javascript" src="https://widget-sandbox.droplabs.pl/bubble.js" id="dl-bubble" data-facility-id="122034" data-bottom="100px" data-right="100px" data-background-color="#b11f20" data-text-color-type="LIGHT" data-href="https://kwojnar137.github.io/" data-text-type="TICKET"  data-shape="bubble"></script>
     </Layout>
   );
 }
