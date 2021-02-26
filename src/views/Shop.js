1a94546d971229c51b5b15a556fd9466f85f897d
kwojnar137 <k.wojnar137@gmail.com>
2021-02-26 15:09:31 +0100

    set obiekt testowy widget

--- src/views/Shop.js
+++ src/views/Shop.js
@@ -15,13 +15,16 @@ const mystyle = {
 
 
 // const style={{border: "0", width: '100%!important', padding: '0', margin: '0 auto', minHeight: '200px', maxWidth: '1000px', display: 'block'}} 
-const script = <div><iframe id="dl-widget" style={mystyle} src='https://widget.droplabs.pl/index.html?facilityId=235&onlineGroupId=5041'></iframe><script type="text/javascript" src="https://widget.droplabs.pl/widget.js"></script></div>
+// const script = <div></div>
 
 export default function Shop() {
   return (
     <Layout>
       {/* {script} */}
-      <script type="text/javascript" src="https://widget-sandbox.droplabs.pl/bubble.js" id="dl-bubble" data-facility-id="122034" data-bottom="100px" data-right="100px" data-background-color="#b11f20" data-text-color-type="LIGHT" data-href="https://kwojnar137.github.io/" data-text-type="TICKET"  data-shape="bubble"></script>
+      {/* <script type="text/javascript" src="https://widget-sandbox.droplabs.pl/bubble.js" id="dl-bubble" data-facility-id="122034" data-bottom="100px" data-right="100px" data-background-color="#b11f20" data-text-color-type="LIGHT" data-href="https://kwojnar137.github.io/" data-text-type="TICKET"  data-shape="bubble"></script> */}
+      {/* <iframe id="dl-widget" src="https://widget-sandbox.droplabs.pl/index.html?facilityId=122034&onlineGroupId=315569"></iframe> */}
+      <iframe id="dl-widget" style={mystyle} src="https://widget-sandbox.droplabs.pl/index.html?facilityId=1&onlineGroupId=3421"></iframe>
+      <script type="text/javascript" src="https://widget-sandbox.droplabs.pl/widget.js"></script>
     </Layout>
   );
 }
